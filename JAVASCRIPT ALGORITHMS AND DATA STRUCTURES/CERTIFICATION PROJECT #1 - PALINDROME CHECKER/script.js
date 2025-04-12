47a7db6cc36f8a443bfdf57a8ea1ad5f90600c16
Saul <saulalbertoortegarodriguez@gmail.com>
2025-04-12 17:38:26 +0100

    Modify file name

---
+++ JAVASCRIPT ALGORITHMS AND DATA STRUCTURES/CERTIFICATION PROJECT #1 - PALINDROME CHECKER/script.js
@@ -0,0 +1,48 @@
+const textInput = document.getElementById('text-input');
+const checkButton = document.getElementById('check-btn');
+const resultDiv = document.getElementById('result');
+
+const cleanInputString = (str) => {
+    const regex = /[\W_]/g;
+    return str.replace(regex, '').toUpperCase();
+}
+
+const reverseWord = (str) => {
+    let reversedString = "";
+    for (let i = str.length - 1; i >= 0; i--) {
+        reversedString += str.charAt(i);
+    }
+
+    return reversedString;
+}
+
+const isAPalindrome = (str) => {
+    const cleanedString = cleanInputString(str);
+    const reversedString = reverseWord(cleanedString);
+    if (str === reversedString) {
+        return true;
+    }
+
+    return false;
+}
+
+const resultMessage = (booleanResult) => {
+    return booleanResult ? ' is a palindrome' : ' is not a palindrome';
+}
+
+const cleanInputTextValue = () => {
+    textInput.value = "";
+}
+
+function printResult() {
+    if (textInput.value.length == 0) {
+        alert('Please input a value');
+    } else {
+        const str = cleanInputString(textInput.value);
+        const printMessage = resultMessage(isAPalindrome(str));
+        resultDiv.innerText = textInput.value + printMessage;
+        cleanInputTextValue();
+    }
+}
+
+checkButton.addEventListener("click", printResult);
